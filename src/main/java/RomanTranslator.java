2374f06767a82400d2a72c83a6741a98f6b03101
albert.renom <albert.renom@fib.upc.edu>
2016-11-03 08:09:39 +0100

    firts commit

--- src/main/java/RomanTranslator.java
+++ src/main/java/RomanTranslator.java
@@ -1,31 +1,11 @@
-import java.util.List;
 
 public class RomanTranslator {
 
-    private List<ArabicRoman> arabicRoman;
-    private int lastPositionUsed;
 
-    public RomanTranslator(List<ArabicRoman> arabicRoman) {
-        this.arabicRoman = arabicRoman;
-        this.lastPositionUsed = 0;
+    public int numberToRoman(int i) {
+
+    return 0;
     }
 
-    public String numberToRoman(Integer n) {
-        int size = arabicRoman.size();
-        while (lastPositionUsed < size) {
-            ArabicRoman conversion = arabicRoman.get(lastPositionUsed);
-            if (n >= conversion.getArabicValue()) return conversion.getRomanValue() + numberToRoman(n - conversion.getArabicValue());
-            ++lastPositionUsed;
-        }
-        return "";
-    }
-
-    /*
-    public String numberToRoman(Integer i) {
-        for (ArabicRoman conversion : arabicRoman) {
-            if (i >= conversion.getArabicValue()) return conversion.getRomanValue() + numberToRoman(i - conversion.getArabicValue());
-        } return "";
-    }
-    */
 
 }
--- src/test/java/RomanNumberTranslatorTest.java
+++ src/test/java/RomanNumberTranslatorTest.java
@@ -1,58 +1,13 @@
 import org.junit.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.Assert.*;
-
 
 public class RomanNumberTranslatorTest {
 
-    private RomanTranslator roman;
-    private List<ArabicRoman> conversionList = initConversionList();
-
-    @Before
-    public void setup() {
-        roman = new RomanTranslator(conversionList);
-    }
-
     @Test public void test1() {
+        RomanTranslator roman;
+        roman = new RomanTranslator();
         assertEquals("I", roman.numberToRoman(1));
-    }
-
-    @Test public void test3() {
-        assertEquals("III", roman.numberToRoman(3));
-    }
-
-    @Test public void test9() {
-        assertEquals("IX", roman.numberToRoman(9));
-    }
-
-    @Test public void test1066() {
-        assertEquals("MLXVI", roman.numberToRoman(1066));
-    }
-
-    @Test public void test1989() {
-        assertEquals("MCMLXXXIX", roman.numberToRoman(1989));
-    }
-
-    private List<ArabicRoman> initConversionList() {
-        List<ArabicRoman> arabicRoman;
-        arabicRoman = new ArrayList<ArabicRoman>();
-        arabicRoman.add(new ArabicRoman(1000, "M"));
-        arabicRoman.add(new ArabicRoman(900, "CM"));
-        arabicRoman.add(new ArabicRoman(500, "D"));
-        arabicRoman.add(new ArabicRoman(400, "CD"));
-        arabicRoman.add(new ArabicRoman(100, "C"));
-        arabicRoman.add(new ArabicRoman(90, "XC"));
-        arabicRoman.add(new ArabicRoman(50, "L"));
-        arabicRoman.add(new ArabicRoman(40, "XL"));
-        arabicRoman.add(new ArabicRoman(10, "X"));
-        arabicRoman.add(new ArabicRoman(9, "IX"));
-        arabicRoman.add(new ArabicRoman(5, "V"));
-        arabicRoman.add(new ArabicRoman(4, "IV"));
-        arabicRoman.add(new ArabicRoman(1, "I"));
-        return arabicRoman;
     }
 
 }
